bccd7eac81727a370ff49a5fd3e7670fd2185b8b
Samson Adejoro <samsonadejoro@gmail.com>
2022-08-08 19:57:54 +0100

    Bug fixes

---
+++ crud-api/Migrations/20220806215714_User migration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace crud_api.Migrations
+{
+    public partial class Usermigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
